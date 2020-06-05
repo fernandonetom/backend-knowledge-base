2cbc1cd8669d13083f510b10e49a3602648eda42
fernando39357 <fernando39357@gmail.com>
2020-06-05 17:55:42 -0300

    Fixed articles list

--- api/article.js
+++ api/article.js
@@ -87,14 +87,14 @@ module.exports = (app) => {
 	const getByCategory = async (req, res) => {
 		const categoryId = req.params.id;
 		const page = req.query.page || 1;
-		const categories = await app.dv.raw(
+		const categories = await app.db.raw(
 			queries.categoryWithChildren,
 			categoryId
 		);
-		const ids = categories.rows.map((category) => category.id);
+		const ids = categories.rows.map((c) => c.id);
 
 		app
-			.db({ a: "articles", b: "users" })
+			.db({ a: "articles", u: "users" })
 			.select("a.id", "a.name", "a.description", "a.imageUrl", {
 				author: "u.name",
 			})
@@ -103,7 +103,7 @@ module.exports = (app) => {
 			.whereRaw("?? = ??", ["u.id", "a.userId"])
 			.whereIn("categoryId", ids)
 			.orderBy("a.id", "desc")
-			.then((articles) => res.status(200).json(articles))
+			.then((articles) => res.json(articles))
 			.catch((err) => res.status(500).send(err));
 	};
 
